d3053a3ee7bef29622f6c5e753b9012da10b1a9a
Brian Maina <brayahnmainah@gmail.com>
2025-05-30 06:36:11 +0300

    shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,12 @@
 import Link from "next/link";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export default function Home() {
   return (
@@ -8,6 +16,17 @@ export default function Home() {
       </h1>
       <h1 className="h1-bold font-inter text-3xl text-light-500">HomePage</h1>
       <Link href={"/sign-in"}> Sign IN</Link>
+      <DropdownMenu>
+        <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem>Billing</DropdownMenuItem>
+          <DropdownMenuItem>Team</DropdownMenuItem>
+          <DropdownMenuItem>Subscription</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </>
   );
 }
